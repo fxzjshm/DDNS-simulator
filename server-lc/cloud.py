d1bbd95aa9f27ae0e144699cc8a82b74d091c087
fxzjshm <fxzjshm@163.com>
2016-06-29 15:02:03 +0800

    Add engine for redirecting on browser.

---
+++ server-lc/cloud.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+from leancloud import Engine
+from leancloud import LeanEngineError
+
+from app import app
+
+
+engine = Engine(app)
+
+
+@engine.define
+def hello(**params):
+    if 'name' in params:
+        return 'Hello, {}!'.format(params['name'])
+    else:
+        return 'Hello, LeanCloud!'
+
