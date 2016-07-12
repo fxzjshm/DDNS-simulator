11182c31e4ac8f7ee2053e098ab8cd94016ff7ce
fxzjshm <fxzjshm@163.com>
2016-07-12 12:35:37 +0800

    Add error render.

--- server/python/app.py
+++ server/python/app.py
@@ -1,5 +1,7 @@
 # coding: utf-8
 
+import sys, traceback
+
 from datetime import datetime
 
 from flask import Flask
@@ -13,9 +15,9 @@ from leancloud import Object
 IP_User_Obj = Object.extend('IP_User_Obj')
 
 app = Flask(__name__)
+query = Query(IP_User_Obj)
 leancloud.client.use_region('CN')
 leancloud.init('CaiXYnmy3BPpcpVE9eRaKQkV-gzGzoHsz','7tryYPnSzMGwpivDanO2yfG0')
-query = Query(IP_User_Obj)
 
 @app.route('/')
 def index():
@@ -27,25 +29,26 @@ def redirectWithObjectId(objectId):
         ip = getIP(objectId)
         return redirect('http://' + ip + "/");
         #return render_template('redirecting.html');
-    except ValueError, e:
+    except Exception, e:
         print e
-        return render_template('index.html')
+        print traceback.format_exc()
+        #print td
+        return render_template('index.html', exception = e)
 
 def getIP(objectId):
     if len(objectId) is 24 :
-        leancloud.init('CaiXYnmy3BPpcpVE9eRaKQkV-gzGzoHsz','7tryYPnSzMGwpivDanO2yfG0')
+        #leancloud.init('CaiXYnmy3BPpcpVE9eRaKQkV-gzGzoHsz','7tryYPnSzMGwpivDanO2yfG0')
         #print objectId
         #print IP_User_Obj._class_name
         #object = query.get(objectId)
-        object = IP_User_Obj.create_without_data(objectId)
-        #object.save()
-        object.fetch()
+        ipObject = IP_User_Obj.create_without_data(objectId)
+        #ipObject.save()
+        ipObject.fetch()
         #if object is None :
         #    return render_template('index.html')
-        ip = object.get('IP')
+        ip = ipObject.get('IP')
         if ip is None :
             raise ValueError, 'Cannot get ip. ' # 'Please check if this object id has been already used.'
         #print ip
     else:
         raise ValueError, 'Invalid object id.'
-
